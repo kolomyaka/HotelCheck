d326c309f22ed5028e1c6d91c24e10db567cab01
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2023-04-09 16:36:51 +0300

    fix on https

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Настраиваем конфиг для axios
 export const $api = axios.create({
-    baseURL: 'http://engine.hotellook.com/api/v2/cache.json',
+    baseURL: 'https://engine.hotellook.com/api/v2/cache.json',
     params: {
         currency:'rub'
     }
